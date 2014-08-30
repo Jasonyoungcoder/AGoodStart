e4f40055ad9cd44a21b1a14877c22921f41d3421
Jason Young <Jasonyoung1010@gmail.com>
2014-08-30 13:33:18 -0600

    Add AGoodStart iPhone project

---
+++ AGoodStart/AGoodStart/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AGoodStart
+//
+//  Created by Jason Young on 8/30/14.
+//  Copyright (c) 2014 DevMountain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ AGoodStart/AGoodStart/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  AGoodStart
+//
+//  Created by Jason Young on 8/30/14.
+//  Copyright (c) 2014 DevMountain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
